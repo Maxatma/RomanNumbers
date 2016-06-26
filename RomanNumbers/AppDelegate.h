2fa7e2c9d8b8c52ce6c7adf1eab6df0be12e8225
Alexander Zaporozhchenko <maxatma@mail.ua>
2016-06-27 02:26:32 +0300

    Initial Commit

---
+++ RomanNumbers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RomanNumbers
+//
+//  Created by Alexander Zaporozhchenko on 6/27/16.
+//  Copyright © 2016 Alexander Zaporozhchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RomanNumbers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RomanNumbers
+//
+//  Created by Alexander Zaporozhchenko on 6/27/16.
+//  Copyright © 2016 Alexander Zaporozhchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
